b9f4653d1923a8c832d3ad9ca1140154d2a0a1eb
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2023-07-01 19:29:46 +0900

    feat: Add TestController "/" returns Hello, World!

---
+++ src/main/kotlin/com/group4/ticketingservice/TicketingserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.group4.ticketingservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketingserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TicketingserviceApplication>(*args)
+}
---
+++ src/main/kotlin/com/group4/ticketingservice/controller/TestController.kt
@@ -0,0 +1,14 @@
+package com.group4.ticketingservice.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/")
+    fun index(): String{
+        println("Hello, World!")
+        return "Hello, World!"
+    }
+
+}
