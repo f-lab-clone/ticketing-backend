d6ee810e3b91a2248ccd785a82d08a335b26ca1a
Park Jeongseop <parkjeongseop@parkjeongseop.com>
2023-07-01 19:48:52 +0900

    Feature/1 project setup (#8)
    * feat: Init SpringBoot Project
    
    * feat: Add TestController
    "/" returns Hello, World!

---
+++ src/test/kotlin/com/group4/ticketingservice/TicketingserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.group4.ticketingservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketingserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
