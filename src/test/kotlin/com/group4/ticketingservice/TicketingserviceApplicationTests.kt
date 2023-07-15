e7bc7a1f904d954144511d51da92fae4d4b95269
ryanbae <bohblue23@gmail.com>
2023-07-15 22:47:34 +0900

    fix: contextLoads() FAILED

--- src/test/kotlin/com/group4/ticketingservice/TicketingserviceApplicationTests.kt
+++ src/test/kotlin/com/group4/ticketingservice/TicketingserviceApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.group4.ticketingservice
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class TicketingserviceApplicationTests {
 
     @Test
