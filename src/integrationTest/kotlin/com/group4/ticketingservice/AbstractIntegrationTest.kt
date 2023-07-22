4c034b30fa42b77aeb9af44d38b265eca39ad749
Junha <junha.ahn.dev@gmail.com>
2023-07-22 10:36:31 +0000

    test: fix testConrailer for manually run

--- src/integrationTest/kotlin/com/group4/ticketingservice/AbstractIntegrationTest.kt
+++ src/integrationTest/kotlin/com/group4/ticketingservice/AbstractIntegrationTest.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
@@ -12,13 +11,13 @@ import org.testcontainers.junit.jupiter.Testcontainers
 abstract class AbstractIntegrationTest {
 
     companion object {
-        @Container
         val mysqlContainer = MySQLContainer("mysql:8.0.33")
             .withConfigurationOverride("./db/mysql/conf.d")
 
         @JvmStatic
         @DynamicPropertySource
         fun datasourceConfig(registry: DynamicPropertyRegistry) {
+            mysqlContainer.start()
             registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
             registry.add("spring.datasource.username", mysqlContainer::getUsername)
             registry.add("spring.datasource.password", mysqlContainer::getPassword)
