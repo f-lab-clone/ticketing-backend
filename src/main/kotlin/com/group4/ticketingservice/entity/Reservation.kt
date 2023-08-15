9d8aad78a604797efbd862aafb14331a9b965ea9
minjun3021 <minjun3021@naver.com>
2023-08-15 20:56:50 +0900

    feat: use lazy loading at @ManyToOne

--- src/main/kotlin/com/group4/ticketingservice/entity/Reservation.kt
+++ src/main/kotlin/com/group4/ticketingservice/entity/Reservation.kt
@@ -2,6 +2,7 @@ package com.group4.ticketingservice.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -15,11 +16,11 @@ class Reservation(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     var user: User,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id", nullable = false)
     var event: Event,
 
