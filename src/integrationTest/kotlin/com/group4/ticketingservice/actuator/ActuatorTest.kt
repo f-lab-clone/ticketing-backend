7d611dd8b36edb3e9fc0fad1e463e43bca6a9d74
MinJun Kim <minjun3021@naver.com>
2023-09-13 20:32:08 +0900

    feat: add dependency

---
+++ src/integrationTest/kotlin/com/group4/ticketingservice/actuator/ActuatorTest.kt
@@ -0,0 +1,23 @@
+package com.group4.ticketingservice.actuator
+
+import com.group4.ticketingservice.AbstractIntegrationTest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@AutoConfigureMockMvc
+class ActuatorTest : AbstractIntegrationTest() {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `get_api_actuator should return json response and it should contains prometheus href`() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/actuator")
+        ).andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$._links").exists())
+    }
+}
