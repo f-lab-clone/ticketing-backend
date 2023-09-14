c727c2216e131759df14eeb6da5d7ce1cbfd7fef
MinJun Kim <minjun3021@naver.com>
2023-09-14 23:39:04 +0900

    feat: add cascade constraint and notnull annotation

--- src/main/kotlin/com/group4/ticketingservice/entity/Event.kt
+++ src/main/kotlin/com/group4/ticketingservice/entity/Event.kt
@@ -1,9 +1,15 @@
 package com.group4.ticketingservice.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.validation.constraints.NotNull
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.time.OffsetDateTime
 
 @Entity
@@ -12,16 +18,31 @@ class Event(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
+    @NotNull
     var title: String,
 
+    @NotNull
     var date: OffsetDateTime,
 
+    @NotNull
     var reservationStartTime: OffsetDateTime,
 
+    @NotNull
     var reservationEndTime: OffsetDateTime,
 
+    @NotNull
     var maxAttendees: Int,
 
+    @NotNull
     var currentReservationCount: Int = 0
 
-)
+) {
+
+    @OneToMany(mappedBy = "event", fetch = FetchType.LAZY, cascade = [ CascadeType.ALL], orphanRemoval = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    val bookmarks: List<Bookmark> = mutableListOf()
+
+    @OneToMany(mappedBy = "event", fetch = FetchType.LAZY, cascade = [ CascadeType.ALL], orphanRemoval = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    val reservations: List<Reservation> = mutableListOf()
+}
