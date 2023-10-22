856abc69e02a9c9050a21770dd4f65c4074e84aa
MinJun Kim <minjun3021@naver.com>
2023-10-22 23:39:40 +0900

    refactor: make querydsl clearly

--- src/main/kotlin/com/group4/ticketingservice/repository/EventRepositorySupport.kt
+++ src/main/kotlin/com/group4/ticketingservice/repository/EventRepositorySupport.kt
@@ -4,6 +4,7 @@ import com.group4.ticketingservice.entity.Event
 import com.group4.ticketingservice.entity.QEvent.event
 import com.group4.ticketingservice.utils.exception.CustomException
 import com.group4.ticketingservice.utils.exception.ErrorCodes
+import com.querydsl.core.types.OrderSpecifier
 import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
@@ -18,41 +19,60 @@ class EventRepositorySupport(
     Event::class.java
 ) {
 
-    private fun ltEventId(id: Int?): BooleanExpression? {
-        return if (id == null) null else event.id.lt(id)
-    }
-
-    fun getEvent(sort: String?, id: Int?, dateTime: OffsetDateTime?): List<Event> {
-        val whereSpecifier = if (id == null) {
-            when (sort) {
-                "deadline" -> event.reservationEndTime.after(OffsetDateTime.now())
-                "startDate" -> event.startDate.before(OffsetDateTime.now() + Duration.ofDays(60))
-                "createdAt" -> null
-                null -> null
-                else -> throw CustomException(ErrorCodes.MESSAGE_NOT_READABLE)
-            }
-        } else {
-            when (sort) {
-                "deadline" -> event.reservationEndTime.gt(dateTime).or(event.reservationEndTime.eq(dateTime).and(ltEventId(id)))
-                "startDate" -> event.startDate.gt(dateTime).or(event.startDate.eq(dateTime).and(ltEventId(id)))
-                "createdAt" -> event.createdAt.lt(dateTime).or(event.createdAt.eq(dateTime).and(ltEventId(id)))
-                null -> ltEventId(id)
-                else -> throw CustomException(ErrorCodes.MESSAGE_NOT_READABLE)
-            }
-        }
-
-        val orderSpecifier = when (sort) {
-            "deadline" -> event.reservationEndTime.asc()
-            "startDate" -> event.startDate.asc()
-            "createdAt" -> event.createdAt.desc()
-            null -> event.id.desc()
-            else -> throw CustomException(ErrorCodes.MESSAGE_NOT_READABLE)
-        }
+    fun getEvent(sortBy: String, id: Int?, dateTime: OffsetDateTime?): List<Event> {
+        val whereSpecifier = buildWhereSpecifier(sortBy, id, dateTime)
+        val orderSpecifier = buildOrderSpecifier(sortBy)
 
         return queryFactory.selectFrom(event)
             .where(whereSpecifier)
             .orderBy(orderSpecifier, event.id.desc())
             .limit(10)
             .fetch()
+    }
+
+    private fun buildWhereSpecifier(sortBy: String?, id: Int?, dateTime: OffsetDateTime?): BooleanExpression? {
+        return when (sortBy) {
+            "deadline" -> buildDeadlineSpecifier(id, dateTime)
+            "startDate" -> buildStartDateSpecifier(id, dateTime)
+            "createdAt" -> buildCreatedAtSpecifier(id, dateTime)
+            else -> throw CustomException(ErrorCodes.MESSAGE_NOT_READABLE)
+        }
+    }
+
+    private fun buildOrderSpecifier(sortBy: String?): OrderSpecifier<*> {
+        return when (sortBy) {
+            "deadline" -> event.reservationEndTime.asc()
+            "startDate" -> event.startDate.asc()
+            "createdAt" -> event.createdAt.desc()
+            else -> event.id.desc()
+        }
+    }
+
+    private fun buildDeadlineSpecifier(id: Int?, dateTime: OffsetDateTime?): BooleanExpression? {
+        return if (id == null) {
+            event.reservationEndTime.after(OffsetDateTime.now())
+        } else {
+            event.reservationEndTime.gt(dateTime).or(event.reservationEndTime.eq(dateTime).and(ltEventId(id)))
+        }
+    }
+
+    private fun buildStartDateSpecifier(id: Int?, dateTime: OffsetDateTime?): BooleanExpression? {
+        return if (id == null) {
+            event.startDate.before(OffsetDateTime.now() + Duration.ofDays(60))
+        } else {
+            event.startDate.gt(dateTime).or(event.startDate.eq(dateTime).and(ltEventId(id)))
+        }
+    }
+
+    private fun buildCreatedAtSpecifier(id: Int?, dateTime: OffsetDateTime?): BooleanExpression? {
+        return if (id == null) {
+            null
+        } else {
+            event.createdAt.lt(dateTime).or(event.createdAt.eq(dateTime).and(ltEventId(id)))
+        }
+    }
+
+    private fun ltEventId(id: Int?): BooleanExpression? {
+        return if (id == null) null else event.id.lt(id)
     }
 }
